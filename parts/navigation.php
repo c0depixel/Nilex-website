edfb9128639854d4f87e6de52356a16eb4c4d764
c0depixel <nenad@codepixel.me>
2017-03-22 22:56:57 +0100

    finish index

--- parts/navigation.php
+++ parts/navigation.php
@@ -2,15 +2,26 @@
   <li>
     <a href="#">Company</a>
   </li>
-  <li class="active">
-    <a href="#">Products</a>
+  <li class="active has-dropdown">
+    <a href="#">Products <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+    <div class="dropdown">
+      <ul>
+        <li><a href="#"><i class="fa fa-external-link" aria-hidden="true"></i> Self Service Portal</a></li>
+        <li><a href="#"><i class="fa fa-external-link" aria-hidden="true"></i> Self Service Portal</a></li>
+        <li><a href="#"><i class="fa fa-external-link" aria-hidden="true"></i> Self Service Portal</a></li>
+        <li><a href="#"><i class="fa fa-external-link" aria-hidden="true"></i> Self Service Portal</a></li>
+        <li><a href="#"><i class="fa fa-external-link" aria-hidden="true"></i> Self Service Portal</a></li>
+        <li><a href="#"><i class="fa fa-external-link" aria-hidden="true"></i> Self Service Portal</a></li>
+        <li><a href="#"><i class="fa fa-external-link" aria-hidden="true"></i> Self Service Portal</a></li>
+      </ul>
+    </div>
   </li>
-  <li>
-    <a href="#">Features</a>
+  <li class="has-dropdown-big">
+    <a href="#">Features <i class="fa fa-angle-down" aria-hidden="true"></i></a>
     <div class="dropdown">
       <div class="container-fluid">
         <div class="row">
-        <div class="col-md-3">
+          <div class="col-md-3">
             <ul>
               <li><a href="#"><i class="fa fa-external-link" aria-hidden="true"></i> Self Service Portal</a></li>
               <li><a href="#"><i class="fa fa-external-link" aria-hidden="true"></i> Self Service Portal</a></li>
